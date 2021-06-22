90b9b7a0e65aaaf3e6abc3beaf93ab8582c53471
Antonio Pontes <anvictorsantos@gmail.com>
2021-06-21 21:59:43 -0300

    fix - titulo renderizado

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,30 +1,18 @@
 <template>
     <base-layout page-title="All Memories">
-        <ion-list>
-            <ion-item v-for="memory in memories" :router-link="`/memories/${memory.id}`" :key="memory.id">
-                <ion-thumbnail slot="start">
-                    <ion-img :src="memory.image" :alt="memory.title"></ion-img>
-                </ion-thumbnail>
-                <ion-label>
-                    {{ memory.title }}
-                </ion-label>
-            </ion-item>
-        </ion-list>
+        <memories-list
+            :memories="memories"
+        >   
+        </memories-list>
     </base-layout>
 </template>
 
 <script>
-import { IonList, IonItem, IonImg, IonThumbnail, IonLabel } from '@ionic/vue'
-import BaseLayout from '../components/base/BaseLayout.vue'
+import MemoriesList from '../components/memories/MemoriesList.vue';
 
 export default {
     components: {
-        IonList, 
-        IonItem,
-        BaseLayout,
-        IonImg,
-        IonThumbnail, 
-        IonLabel
+        MemoriesList,
     },
     computed: {
         memories() {
